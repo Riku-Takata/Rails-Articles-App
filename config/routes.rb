aafbc69938845c9c7fcf4a8c8217a6819a757585
Riku-Takata <riku_takata@icloud.com>
2024-08-20 11:02:31 +0900

    change rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'articles#index'
-  resources :articles, only: [:index, :show, :create, :update, :destroy]
+  root "articles#index"
+  resources :articles, only: [ :index, :show, :create, :update, :destroy ]
 end
